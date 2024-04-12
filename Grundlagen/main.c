6e3b22e0fbcacdb6eb9c69673d377322557e4526
Jo Wagner <jowagner@limago.de>
2024-04-12 10:57:40 +0200

    initial commit

--- Grundlagen/main.c
+++ Grundlagen/main.c
@@ -1,35 +1,17 @@
 #include <stdio.h>
 
-void ausgabe(int * array, size_t size) {
-    for (int i = 0; i < size; ++i) {
-        printf("%d\n",array[i]);// feld + i * sizeof(int)
-    }
+size_t stringlenght(char * message) {
+    return 0;
 }
 
 int main() {
 
-    int feld [] = {10,20,30,40};
+   char buffer[] = "Hallo";
+   //buffer[5] = 'x';
 
-    int * ptr;
 
-    ptr = feld;
 
-    printf("%d\n", *feld);
-    printf("%d\n", feld[0]);
-
-    printf("%d\n", *ptr);
-    printf("%d\n", ptr[0]);
-
-    printf("%d\n", feld[1]);
-    printf("%d\n", *(feld + 1));
-
-//    for (int i = 0; i < 4; ++i) {
-//       printf("%d\n",feld[i]);// feld + i * sizeof(int)
-//    }
-
-    ausgabe(feld, 4);
-    printf("%d\n", sizeof(feld));
-    printf("%d\n", sizeof(ptr));
+    printf("%d\n", stringlenght(buffer));
 
     return 0;
 }
